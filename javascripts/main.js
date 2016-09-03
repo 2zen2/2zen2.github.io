db2446c43e3c2b7dfaef8cbb026bff3d0ab4a2f1
2zen2 <zeny@live.unc.edu>
2016-09-03 02:49:45 -0400

    switch it up

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,53 +1,59 @@
-/** @jsx React.DOM */
+var QuadraticCalculator = React.createClass({
+  getInitialState: function() {
+    return {
+      a: 1,
+      b: 3,
+      c: -4
+    };
+  },
 
-var List = React.createClass({
-  render: function(){
-    return (
-      <ul>
-      {
-        this.props.items.map(function(item) {
-          return <li key={item}>{item}</li>
-        })
-       }
-      </ul>
-    )  
-  }
-});
+  /**
+   * This function will be re-bound in render multiple times. Each .bind() will
+   * create a new function that calls this with the appropriate key as well as
+   * the event. The key is the key in the state object that the value should be
+   * mapped from.
+   */
+  handleInputChange: function(key, event) {
+    var partialState = {};
+    partialState[key] = parseFloat(event.target.value);
+    this.setState(partialState);
+  },
 
-var FilteredList = React.createClass({
-  filterList: function(event){
-    var updatedList = this.state.initialItems;
-    updatedList = updatedList.filter(function(item){
-      return item.toLowerCase().search(event.target.value.toLowerCase()) !== -1;
-    });
-    this.setState({items: updatedList});
-  },
-  getInitialState: function(){
-     return {
-       initialItems: [
-         "Apples",
-         "Broccoli",
-         "Chicken",
-         "Duck",
-         "Eggs",
-         "Fish",
-         "Granola",
-         "Hash Browns"
-       ],
-       items: []
-     }
-  },
-  componentWillMount: function(){
-    this.setState({items: this.state.initialItems})
-  },
-  render: function(){
+  render: function() {
+    var a = this.state.a;
+    var b = this.state.b;
+    var c = this.state.c;
+    var root = Math.sqrt(Math.pow(b, 2) - 4 * a * c);
+    var denominator = 2 * a;
+    var x1 = (-b + root) / denominator;
+    var x2 = (-b - root) / denominator;
     return (
-      <div className="filter-list">
-        <input type="text" placeholder="Search" onChange={this.filterList}/>
-      <List items={this.state.items}/>
+      <div>
+        <strong>
+          <em>ax</em><sup>2</sup> + <em>bx</em> + <em>c</em> = 0
+        </strong>
+        <h4>Solve for <em>x</em>:</h4>
+        <p>
+          <label>
+            a: <input type="number" value={a} onChange={this.handleInputChange.bind(null, 'a')} />
+          </label>
+          <br />
+          <label>
+            b: <input type="number" value={b} onChange={this.handleInputChange.bind(null, 'b')} />
+          </label>
+          <br />
+          <label>
+            c: <input type="number" value={c} onChange={this.handleInputChange.bind(null, 'c')} />
+          </label>
+          <br />
+          x: <strong>{x1}, {x2}</strong>
+        </p>
       </div>
     );
   }
 });
 
-React.renderComponent(<FilteredList/>, document.getElementById('mount-point'));
+ReactDOM.render(
+  <QuadraticCalculator />,
+  document.getElementById('container')
+);

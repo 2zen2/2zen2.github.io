6aac40944077b83b3bc8fb871715d05f70f731b8
2zen2 <zeny@live.unc.edu>
2016-10-07 03:05:06 -0400

    reset css for different browsers

--- js/main.js
+++ js/main.js
@@ -62,6 +62,11 @@ onePageScroll(".main", {
    }
 
    /*todo pic expand*/
+   document.getElementById("z").onclick = function(){loadImage()};
+}
+
+function loadImage() {
+   alert("Z");
 }
 
 function createParticles() {
